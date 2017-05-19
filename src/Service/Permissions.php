9ab2cd8ec67c76c8a5f10eff0f61c616b4da679d
James Jervis <jjervis@relivinc.com>
2017-05-19 18:35:23 -0500

    Convert to php files

---
+++ src/Service/Permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jerv\ServerEnvironment\Service;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+class Permissions
+{
+    const DEFAULT_FOLDER = 0755;
+    const DEFAULT_FILE = 0655;
+}
